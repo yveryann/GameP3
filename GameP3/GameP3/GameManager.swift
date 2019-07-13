1dc1674c168f3ba4f2e4707e34529ee59a5dcbc7
Yann Yver <yveryann@gmail.com>
2019-07-13 17:38:26 +0200

    update Team building in GameManager

--- GameP3/GameP3/GameManager.swift
+++ GameP3/GameP3/GameManager.swift
@@ -8,13 +8,14 @@
 class GameManager {
     
     var teams = [Team]()
-    var players = [Character]()
+    // Delete array and use array of Team
+    
     
     func getStringFromUser() -> String {
         let userChoice = readLine()
         if let choice = userChoice { //verify that the unpacking has succeeded: choice!=nil
             return choice
-        } else { // unpacking has failed so choice=nil
+        } else { // unpacking has failed so choice=nil  - Function does not display the error and crash the program
             print("Erreur de saisie !")
             return ""
         }
@@ -22,8 +23,13 @@ class GameManager {
     
     func getIntFromUser() -> Int {
         if let userChoice = readLine() {
-            return Int(userChoice)!
-        } else {
+            if let i = Int(userChoice) {
+                return i
+            } else {
+                print("Erreur de saisie")
+                return 0
+            }
+        } else {   //Function does not display the error and crash the program
             print("Erreur de saisie")
             return 0
         }
@@ -48,25 +54,48 @@ class GameManager {
     }
     
     func createTeamName() {
-        
+        var choosenNames = [String]()
         for i in 1...2 {
-            
-            print("Entrer le nom de l'équipe \(i):")
-            let input = getStringFromUser()
-            let team = Team(teamName: input, players: players)
+            var input : String
+            repeat {
+                print("Entrer un nom unique pour l'équipe \(i):")
+                input = getStringFromUser()
+            } while choosenNames.contains(input)
+            choosenNames.append(input)
+            let team = Team(teamName: input, players: [Character]())
             teams.append(team)
+            
         }
-        print(teams)
-        createPlayers()
+        print()
+        createPlayers(teamIndex: 0)
+        print()
+        print("\n   Félicitation l'équipe: \(teams[0].teamName) est crée.\n")
+        print("Les personnages sont: \(teams[0].players[0].name)")
+        print()
+        //print(" \(players[0].name): \(players[0].warriorType) arme: \(players[0].weapon) niveau de vie: \(players[0].life) \n \(players[1].name): \(players[1].warriorType) arme: \(players[1].weapon) niveau de vie: \(players[1].life) \n \(players[2].name): \(players[2].warriorType) arme: \(players[2].weapon) niveau de vie: \(players[2].life)")
+        
+        createPlayers(teamIndex: 1)
+        print()
+        print("\n   Félicitation l'équipe: \(teams[1].teamName) est crée.\n")
+        print("Les personnages sont:\(teams[1].players[1].name)")
+        print()
+        // print(" \(players[3].name): \(players[3].warriorType) arme: \(players[3].weapon) niveau de vie: \(players[3].life) \n \(players[4].name):  \(players[4].warriorType) arme: \(players[4].weapon) niveau de vie: \(players[4].life) \n \(players[5].name):  \(players[5].warriorType) arme: \(players[5].weapon) niveau de vie: \(players[5].life)")
+        
     }
     
     
-    func createPlayers() {
-        
+    func createPlayers(teamIndex: Int) {
+        //ajouter ligne virtuel teamIndex=0
+        var choosenName = [String]()
+        var arrayOfCharacter = [Character]() //create array of character var arrayOfCharacters = [Character]()
         for i in 1...3 {
+            var input: String
             var choiceUserOne = Int()
-            print("Entrer le nom de votre personnage\(i):")
-            let input = getStringFromUser()
+            repeat {
+                print("Entrer un nom unique de votre personnage\(i):")
+                input = getStringFromUser()
+            } while choosenName.contains(input)
+            choosenName.append(input)
             repeat {
                 print("Choix personnage \(i) :")
                 print("1. Fighter")
@@ -93,9 +122,13 @@ class GameManager {
                 break
             }
             let tempPlayers = Character(name: input, warriorType: warriorOne)
-            players.append(tempPlayers)
+            arrayOfCharacter.append(tempPlayers) //add tempPlayers in arrayOfCharacters
+            //teams[teamIndex].players.append(tempPlayers) //c'est une deuxième manière de faire sans passer par la ligne 124
+            // soit on garde ligne 119 + 125 soit on utilise juste la ligne 120
+            
         }
-            print(players)
+        teams[teamIndex].players = arrayOfCharacter
+        //assign arrayOfCharacters to the team index teamIndex
+        
     }
-
 }
